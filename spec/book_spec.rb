9557783dcb4e24aab49922ed4ca1fd5d979af53b
jai-jk <jay.jk@hotmail.com>
2021-01-30 18:39:25 +0000

    Add 5 passing tests

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,1 +1,37 @@
-require './spec_helper'
+require 'spec_helper'
+
+describe Book do
+
+    before :each do
+        @book = Book.new "Title", "Author", :category
+    end
+
+    describe "#new" do
+        it "returns a new book object" do
+            @book.should be_an_instance_of Book
+        end
+
+        it "throws an ArgumentError when given fewer than 3 parameters" do
+            lambda { Book.new "Title", "Author" }.should raise_exception ArgumentError
+        end
+
+    end
+
+    describe "#title" do
+        it "returns the correct title" do
+            @book.title.should eq "Title"
+        end
+    end
+
+    describe "#title" do
+        it "returns the correct author" do
+            @book.author.should eq "Author"
+        end
+    end
+
+    describe "#title" do
+        it "returns the correct category" do
+            @book.category.should eq :category
+        end
+    end
+end
